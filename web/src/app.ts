1064212b557c6f5d87da810bf5d10c729feb56e0
Tom Artiom Fiodorov <a.fiodorov@yahoo.co.uk>
2022-12-31 17:18:35 +0000

    getting rid of catch-all

--- web/src/app.ts
+++ web/src/app.ts
@@ -1,10 +1,12 @@
 import { AppManager } from './app_manager';
+import { chainValues } from './config';
 
 if (typeof (window as any).ethereum !== 'undefined') {
   const chainID = await (window as any).ethereum.request({ method: 'eth_chainId' });
-  try {
+
+  if (chainValues.has(chainID)) {
     new AppManager(chainID).init();
-  } catch (error) {
+  } else {
     const thoughtsContainer = document.getElementById('thoughts-container')!;
     thoughtsContainer.innerHTML = '<h1>Unsupported network</h1>';
   }
