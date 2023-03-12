1b8b1a42cb8adf202e14bc70ad7f07b96a53bd7b
Muhammad Azam <mazam8414@gmail.com>
2023-03-12 22:05:40 +0500

    Test

--- web/src/thought.ts
+++ web/src/thought.ts
@@ -82,7 +82,9 @@ export function makeThoughtContainer(t: ThoughtEntity, appManager: AppManager): 
 
   const dateElementLink = document.createElement('div');
   dateElementLink.classList.add('thought-date-link');
-  dateElementLink.innerHTML = `<a href='?thought-id=${t.id}'>📅</a>`;
+  dateElementLink.innerHTML = `<a href='?thought-id=${t.id}'>
+  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#2b2b2b" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
+  </a>`;
 
   dateElement.appendChild(dateElementLink);
   dateElement.appendChild(dateElementText);
