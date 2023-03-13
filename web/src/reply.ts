adef09af74f1599527457539576139bdd6623b4d
Muhammad Azam <mazam8414@gmail.com>
2023-03-13 15:51:04 +0500

    fix icons for mobile

--- web/src/reply.ts
+++ web/src/reply.ts
@@ -73,7 +73,7 @@ export function makeReplyContainer(
   const dateElementLink = document.createElement('div');
   dateElementLink.classList.add('reply-date-link');
   dateElementLink.innerHTML = `<a href='?reply-id=${r.id}'>
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
+  <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" fill="none" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
   </a>`;
 
   dateElement.appendChild(dateElementLink);
@@ -110,7 +110,7 @@ export function makeReplyContainer(
     const quoteElementLink = document.createElement('a');
     quoteElementLink.href = '#';
     quoteElementLink.innerHTML =
-      '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"  stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-refresh-ccw"><polyline points="1 4 1 10 7 10"></polyline><polyline points="23 20 23 14 17 14"></polyline><path d="M20.49 9A9 9 0 0 0 5.64 5.64L1 10m22 4l-4.64 4.36A9 9 0 0 1 3.51 15"></path></svg>';
+      '<svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" fill="none"  stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-refresh-ccw"><polyline points="1 4 1 10 7 10"></polyline><polyline points="23 20 23 14 17 14"></polyline><path d="M20.49 9A9 9 0 0 0 5.64 5.64L1 10m22 4l-4.64 4.36A9 9 0 0 1 3.51 15"></path></svg>';
     quoteElementLink.setAttribute('reply-id', r.id);
     quoteElementLink.addEventListener('click', (_) =>
       toggleDialogue(null, r.id, appManager.metaMask!, appManager)
@@ -120,7 +120,7 @@ export function makeReplyContainer(
   } else {
     const quoteElementNoLink = document.createElement('div');
     quoteElementNoLink.innerHTML =
-      '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"  stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-refresh-ccw"><polyline points="1 4 1 10 7 10"></polyline><polyline points="23 20 23 14 17 14"></polyline><path d="M20.49 9A9 9 0 0 0 5.64 5.64L1 10m22 4l-4.64 4.36A9 9 0 0 1 3.51 15"></path></svg>';
+      '<svg xmlns="http://www.w3.org/2000/svg"   viewBox="0 0 24 24" fill="none"  stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-refresh-ccw"><polyline points="1 4 1 10 7 10"></polyline><polyline points="23 20 23 14 17 14"></polyline><path d="M20.49 9A9 9 0 0 0 5.64 5.64L1 10m22 4l-4.64 4.36A9 9 0 0 1 3.51 15"></path></svg>';
 
     quoteElement.appendChild(quoteElementNoLink);
   }
