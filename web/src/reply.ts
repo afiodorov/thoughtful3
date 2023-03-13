6f30d36626705b0feea1668c9482d3cd427f4cd3
Muhammad Azam <mazam8414@gmail.com>
2023-03-13 06:50:37 +0500

    update reply

--- web/src/reply.ts
+++ web/src/reply.ts
@@ -73,7 +73,9 @@ export function makeReplyContainer(
 
   const dateElementLink = document.createElement('div');
   dateElementLink.classList.add('reply-date-link');
-  dateElementLink.innerHTML = `<a href='?reply-id=${r.id}'>📅</a>`;
+  dateElementLink.innerHTML = `<a href='?reply-id=${r.id}'>
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg> 
+  </a>`;
 
   dateElement.appendChild(dateElementLink);
   dateElement.appendChild(dateElementText);
@@ -81,11 +83,11 @@ export function makeReplyContainer(
   const likeElement = document.createElement('div');
   likeElement.classList.add('reply-like');
   if (appManager.metaMask == null) {
-    likeElement.textContent = `❤`;
+    likeElement.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"   stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>`;
   } else {
     const likeElementLink = document.createElement('a');
     likeElementLink.href = '#';
-    likeElementLink.textContent = `❤`;
+    likeElementLink.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"   stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>`;
     likeElementLink.classList.add('reply-like-link');
     likeElementLink.setAttribute('reply-id', r.id);
     likeElementLink.addEventListener('click', (event) =>
@@ -108,7 +110,7 @@ export function makeReplyContainer(
   if (appManager.metaMask) {
     const quoteElementLink = document.createElement('a');
     quoteElementLink.href = '#';
-    quoteElementLink.textContent = '🔄';
+    quoteElementLink.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"  stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-refresh-ccw"><polyline points="1 4 1 10 7 10"></polyline><polyline points="23 20 23 14 17 14"></polyline><path d="M20.49 9A9 9 0 0 0 5.64 5.64L1 10m22 4l-4.64 4.36A9 9 0 0 1 3.51 15"></path></svg>';
     quoteElementLink.setAttribute('reply-id', r.id);
     quoteElementLink.addEventListener('click', (event) =>
       toggleDialogue(event, appManager.metaMask!, appManager)
@@ -117,7 +119,7 @@ export function makeReplyContainer(
     quoteElement.appendChild(quoteElementLink);
   } else {
     const quoteElementNoLink = document.createElement('div');
-    quoteElementNoLink.textContent = '🔄';
+    quoteElementNoLink.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"  stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-refresh-ccw"><polyline points="1 4 1 10 7 10"></polyline><polyline points="23 20 23 14 17 14"></polyline><path d="M20.49 9A9 9 0 0 0 5.64 5.64L1 10m22 4l-4.64 4.36A9 9 0 0 1 3.51 15"></path></svg>';
 
     quoteElement.appendChild(quoteElementNoLink);
   }
@@ -135,11 +137,11 @@ export function makeReplyContainer(
   replyElement.classList.add('thought-reply');
   if (isLast) {
     if (!appManager.metaMask) {
-      replyElement.textContent = `💬`;
+      replyElement.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-message-square"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path></svg> `;
     } else {
       const replyElementLink = document.createElement('a');
       replyElementLink.href = `#new-reply-${r.thought}`;
-      replyElementLink.textContent = `💬`;
+      replyElementLink.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-message-square"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path></svg>`;
       replyElementLink.classList.add('thought-reply-icon');
       replyElementLink.setAttribute('thought-id', r.thought);
       replyElementLink.addEventListener('click', (event) => toggleNewReply(event, appManager));
