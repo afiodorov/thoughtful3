6af8180de47754343d37dbb8d96be5a672bc1e10
Muhammad Azam <mazam8414@gmail.com>
2023-03-13 20:17:59 +0500

    popup style

--- web/src/new_reply.ts
+++ web/src/new_reply.ts
@@ -74,7 +74,7 @@ export function makeNewReplyContainer(thoughtID: string, appManager: AppManager)
   const publish = document.createElement('a');
   publish.classList.add('new-reply-publish');
   publish.href = `#new-reply-${thoughtID}`;
-  publish.textContent = '📧';
+  publish.textContent = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#2b2b2b" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>';
   publish.id = `new-reply-publish-${thoughtID}`;
   publish.addEventListener('click', (event) =>
     publishReply(event, thoughtID, appManager.metaMask!, appManager)
