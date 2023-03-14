4a9bb2e66ff6870b9f6ffc50e29b4f0cfb0f0544
Tom Artiom Fiodorov <a.fiodorov@yahoo.co.uk>
2023-03-14 07:53:11 +0000

    switching network programmatically

--- web/src/app.ts
+++ web/src/app.ts
@@ -7,8 +7,23 @@ if (typeof (window as any).ethereum !== 'undefined') {
   if (chainValues.has(chainID) && chainValues.get(chainID)?.enabled) {
     new AppManager(chainID).init();
   } else {
-    const thoughtsContainer = document.getElementById('thoughts-container')!;
-    thoughtsContainer.innerHTML = '<h1>Unsupported network: switch to Polygon</h1>';
+    try {
+      await (window as any).ethereum.request({
+        method: 'wallet_switchEthereumChain',
+        params: [{ chainId: '0x89' }]
+      });
+
+      new AppManager('0x89').init();
+    } catch (switchError) {
+      const thoughtsContainer = document.getElementById('thoughts-container')!;
+      thoughtsContainer.innerHTML = '<h1>Unsupported network: switch to Polygon</h1>';
+
+      // This error code indicates that the chain has not been added to MetaMask.
+      if (switchError.code === 4902) {
+        console.log('This network is not available in your metamask, please add it');
+      }
+      console.log('Failed to switch to the network');
+    }
   }
 } else {
   new AppManager('0x89').init();
